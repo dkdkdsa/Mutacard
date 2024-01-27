5fa77384534d438a0507a3f682126135d51f0d39
JangWheesung <gameplay1qaz@gmail.com>
2024-01-27 17:08:20 +0900

    대충 도감UI

---
+++ Assets/02_Script/Wheesung/EncyclopediaData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct EncyclopediaData
+{
+    Sprite sprite;
+    string name;
+    string explanation;
+}
