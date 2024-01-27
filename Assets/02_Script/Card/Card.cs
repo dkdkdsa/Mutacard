781a8d5a57d1cfbdf2a77b31babf8964a766a3e5
dkdkdsa <bos94330@gmail.com>
2024-01-27 18:20:08 +0900

    card

--- Assets/02_Script/Card/Card.cs
+++ Assets/02_Script/Card/Card.cs
@@ -1,8 +1,76 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+
+public enum CardType
+{
+
+    Item,
+
+}
+
+[Serializable]
+public class CardMargeData
+{
+
+    public CardDataSO targetData;
+    public Card cardPrefab;
+
+}
 
 public abstract class Card : MonoBehaviour
 {
+
+    [SerializeField] private LanguageType debugView;
+    [SerializeField] private List<CardMargeData> margeDatas;
+
+    [field:SerializeField] public CardDataSO data { get; private set; }
+
+    protected SpriteRenderer iconRenderer;
+    protected TMP_Text nameText;
+
+    private void Awake()
+    {
+        
+        nameText = transform.Find("NameText").GetComponent<TMP_Text>();
+        iconRenderer = transform.Find("Icon").GetComponent<SpriteRenderer>();
+
+        HandleLanguageChange();
+
+        LanguageManager.OnLanguageChangeEvent += HandleLanguageChange;
+
+    }
+
+    private void HandleLanguageChange()
+    {
+
+        nameText.text = data.cardName.Text;
+        iconRenderer.sprite = data.icon;
+
+    }
+
+    private void OnDestroy()
+    {
+
+        LanguageManager.OnLanguageChangeEvent -= HandleLanguageChange;
+
+    }
+
+
+#if UNITY_EDITOR
+
+    protected virtual void OnValidate()
+    {
+
+        if (data == null) return;
+
+        transform.Find("NameText").GetComponent<TMP_Text>().text = data.cardName[debugView];
+        transform.Find("Icon").GetComponent<SpriteRenderer>().sprite =data.icon;
+
+    }
+
+#endif
 
 }
---
+++ Assets/02_Script/Card/CardDataSO.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/Card/Data")]
+public class CardDataSO : ScriptableObject
+{
+
+    [field:SerializeField] public LanguageData cardName { get; private set; }
+    [field:SerializeField] public CardType cardType { get; private set; }
+    [field:SerializeField] public Sprite icon { get; private set; }
+    [field:SerializeField] public LanguageData cardExplanation { get; private set; }
+
+}
