6d32fbff577d4a78e9476ae21714e2c692b107b1
JangWheesung <gameplay1qaz@gmail.com>
2024-01-28 05:05:26 +0900

    버튼 매니저

--- Assets/02_Script/UI/Button/ButtonManager.cs
+++ Assets/02_Script/UI/Button/ButtonManager.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static ButtonManager Instance;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(transform);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void ActiveTrueObj(GameObject obj)
+        => obj.SetActive(true);
+
+    public void ActiveFalseObj(GameObject obj)
+        => obj.SetActive(false);
+
+    public void ActiveReversObj(GameObject obj)
+        => obj.SetActive(!obj.activeSelf);
+
+    public void LoadScene(string name)
+        => SceneManager.LoadScene(name);
+
+    public void QuitGame()
+        => Application.Quit();
 }
