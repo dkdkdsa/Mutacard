2e8b9db7ba624f0afb337b4e3e08db00ae955490
dkdkdsa <bos94330@gmail.com>
2024-01-28 21:09:32 +0900

    asdfasdf

---
+++ Assets/02_Script/Card/CardSell.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSell : MonoBehaviour
+{
+
+    private Card targetCard;
+
+    private void Update()
+    {
+
+        if (Input.GetMouseButtonUp(0))
+        {
+
+            if(targetCard != null)
+            {
+
+                MoneyManager.instance.AddMoney(targetCard.data.rank * 3);
+                Destroy(targetCard.gameObject);
+
+            }
+
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("MargeBox"))
+        {
+
+            targetCard = other.transform.GetComponentInParent<Card>();
+
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.CompareTag("MargeBox"))
+        {
+
+            targetCard = null;
+
+        }
+
+
+    }
+
+}
